b8cd88c881e397253e70c9308282a372e5357403
Akulbhard <68096426+Akulbhard@users.noreply.github.com>
2020-08-27 13:55:29 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,24 @@
+class bob {
+    constructor(x, y,radius) {
+      var options = {
+          'restitution':0.3,
+          'isStatic' :true,
+          'friction':0.5,
+          
+          'density':1.2
+      }
+      this.body = Matter.Bodies.circle(x,y, radius,options);
+      this.radius = radius;
+      
+      
+      World.add(world, this.body);
+    }
+    display(){
+      
+      
+      
+      fill("purple");
+      ellipseMode(CENTER);
+      ellipse( this.body.position.x, this.body.position.y, this.radius, this.radius);
+    }
+}
---
+++ roof.js
@@ -0,0 +1,23 @@
+class roof{
+    constructor(x,y,width,height,box1,box2,box3) {
+        var options = {
+            isStatic: true
+            
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+
+       
+       
+        
+        World.add(world, this.body);
+      }
+      display(){
+        
+        
+        fill(0);
+        rectMode(CENTER);
+        rect(this.body.position.x, this.body.position.y, this.width, this.height);
+      }
+    }
